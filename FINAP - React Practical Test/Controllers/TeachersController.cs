2b50cce786cfc1ceaa4d0dbcb4aef0faab22d6c9
Himesh Perera <himesh@wow.com>
2022-07-30 22:51:39 +0530

    Controlers

---
+++ FINAP - React Practical Test/Controllers/TeachersController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FINAP___React_Practical_Test;
+using FINAP___React_Practical_Test.Models;
+
+namespace FINAP___React_Practical_Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeachersController : ControllerBase
+    {
+        private readonly FinAPReactPracticalTestContext _context;
+
+        public TeachersController(FinAPReactPracticalTestContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Teachers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Teacher>>> GetTeachers()
+        {
+            return await _context.Teachers.ToListAsync();
+        }
+
+        // GET: api/Teachers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Teacher>> GetTeacher(int id)
+        {
+            var teacher = await _context.Teachers.FindAsync(id);
+
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            return teacher;
+        }
+
+        // PUT: api/Teachers/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTeacher(int id, Teacher teacher)
+        {
+            if (id != teacher.TeacherID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(teacher).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeacherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Teachers
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Teacher>> PostTeacher(Teacher teacher)
+        {
+            _context.Teachers.Add(teacher);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TeacherExists(teacher.TeacherID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTeacher", new { id = teacher.TeacherID }, teacher);
+        }
+
+        // DELETE: api/Teachers/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Teacher>> DeleteTeacher(int id)
+        {
+            var teacher = await _context.Teachers.FindAsync(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            _context.Teachers.Remove(teacher);
+            await _context.SaveChangesAsync();
+
+            return teacher;
+        }
+
+        private bool TeacherExists(int id)
+        {
+            return _context.Teachers.Any(e => e.TeacherID == id);
+        }
+    }
+}
