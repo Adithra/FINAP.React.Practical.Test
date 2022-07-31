88a4154449dfac2c0cc2b3b4f8028b1072f00ccc
Himesh Perera <himesh@wow.com>
2022-08-01 01:10:56 +0530

    commonform

--- finap_react_practical_test_ui/src/components/commonform.js
+++ finap_react_practical_test_ui/src/components/commonform.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const useForm = (initialFieldValues, validate, setCurrentId) => {
+const commonform = (initialFieldValues, validate, setCurrentId) => {
     const [values, setValues] = useState(initialFieldValues)
     const [errors, setErrors] = useState({})
 
@@ -31,3 +31,5 @@ const useForm = (initialFieldValues, validate, setCurrentId) => {
         resetForm
     };
 }
+
+export default commonform;
