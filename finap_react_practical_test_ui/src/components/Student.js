f9b66abb0975fef57b85178c39a77fcdbc850779
Himesh Perera <himesh@wow.com>
2022-08-01 01:53:00 +0530

    firstName

--- finap_react_practical_test_ui/src/components/Student.js
+++ finap_react_practical_test_ui/src/components/Student.js
@@ -42,7 +42,7 @@ const Student = ({classes, ...props }) => {
                         <Grid container>
                             <Grid item xs={6}>
                                 <TextField fullWidth
-                                    name="firstName"
+                                    name="fName"
                                     variant="outlined"
                                     label="First Name"
                                     value={values.firstName}
