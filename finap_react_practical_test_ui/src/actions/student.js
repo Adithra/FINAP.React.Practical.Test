be7fffa9eb99c08a5cce1473ab17523dd353fa96
Himesh Perera <himesh@wow.com>
2022-08-01 02:44:57 +0530

    student

--- finap_react_practical_test_ui/src/actions/student.js
+++ finap_react_practical_test_ui/src/actions/student.js
@@ -27,11 +27,36 @@ export const fetchAll = () => dispatch => {
 
 export const create = (data, onSuccess) => dispatch => {
     data = formateData(data)
-    api.dCandidate().create(data)
+    api.student().create(data)
         .then(res => {
             dispatch({
                 type: ACTION_TYPES.CREATE,
                 payload: res.data
+            })
+            onSuccess()
+        })
+        .catch(err => console.log(err))
+}
+
+export const update = (id, data, onSuccess) => dispatch => {
+    data = formateData(data)
+    api.student().update(id, data)
+        .then(res => {
+            dispatch({
+                type: ACTION_TYPES.UPDATE,
+                payload: { id, ...data }
+            })
+            onSuccess()
+        })
+        .catch(err => console.log(err))
+}
+
+export const Delete = (id, onSuccess) => dispatch => {
+    api.student().delete(id)
+        .then(res => {
+            dispatch({
+                type: ACTION_TYPES.DELETE,
+                payload: id
             })
             onSuccess()
         })
