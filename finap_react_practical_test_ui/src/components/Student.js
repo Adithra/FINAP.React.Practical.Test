63652343206978ce2a0fc2ee9d4a7375030e36db
Himesh Perera <himesh@wow.com>
2022-08-03 22:33:54 +0530

    HF

--- finap_react_practical_test_ui/src/components/Student.js
+++ finap_react_practical_test_ui/src/components/Student.js
@@ -68,97 +68,95 @@ const Student = ({classes, ...props }) => {
     }
 
     return (
-                    <form autoComplete="off" noValidate className={theme.root} onSubmit={handleSubmit}>
-                        <Grid container>
-                            <Grid item xs={6}>
-                                <TextField fullWidth
-                                    name="firstName"
-                                    variant="outlined"
-                                    label="First Name"
-                                    value={values.firstName}
-                                    onChange={handleInputChange}
-                                    {...(errors.email && { error: true, helperText: errors.firstName })}
-                                />
-                            </Grid>
-                            <Grid item xs={6} >
-                                <TextField fullWidth
-                                    name="lastName"
-                                    variant="outlined"
-                                    label="Last Name"
-                                    value={values.lastName}
-                                    onChange={handleInputChange}
-                                    {...(errors.email && { error: true, helperText: errors.lastName })}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField fullWidth
-                                    name="contactPerson"
-                                    variant="outlined"
-                                    label="Contact Person"
-                                    value={values.contactPerson}
-                                    onChange={handleInputChange}
-                                    {...(errors.email && { error: true, helperText: errors.contactPerson })}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField fullWidth
-                                    name="contactNo"
-                                    variant="outlined"
-                                    label="Contact No"
-                                    value={values.contactNo}
-                                    onChange={handleInputChange}
-                                    {...(errors.email && { error: true, helperText: errors.contactNo })}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField fullWidth
-                                    name="emailAddress"
-                                    variant="outlined"
-                                    label="Email Address"
-                                    value={values.emailAddress}
-                                    onChange={handleInputChange}
-                                    {...(errors.email && { error: true, helperText: errors.emailAddress })}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField fullWidth
-                                    name="dateOfBirth"
-                                    variant="outlined"
-                                    label="Date of Birth"
-                                    value={values.dateOfBirth}
-                                    onChange={handleInputChange}
-                                    {...(errors.email && { error: true, helperText: errors.dateOfBirth })}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField fullWidth
-                                    name="age"
-                                    variant="outlined"
-                                    label="Age"
-                                    value={values.age}
-                                    onChange={handleInputChange}
-                                    {...(errors.email && { error: true, helperText: errors.age })}
-                                />
-                            </Grid>
-                            <div>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    type="submit"
-                                >
-                                    Submit
-                                </Button>
-                                <Button
-                                    variant="contained"
-                                    onClick={resetForm}
-                                >
-                                    Reset
-                                </Button>
-                            </div>
-                        </Grid>
-                    </ form>
-                
-            
+        <form autoComplete="off" noValidate className={theme.root} onSubmit={handleSubmit}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <TextField fullWidth
+                        name="firstName"
+                        variant="outlined"
+                        label="First Name"
+                        value={values.firstName}
+                        onChange={handleInputChange}
+                        {...(errors.email && { error: true, helperText: errors.firstName })}
+                    />
+                </Grid>
+                <Grid item xs={6} >
+                    <TextField fullWidth
+                        name="lastName"
+                        variant="outlined"
+                        label="Last Name"
+                        value={values.lastName}
+                        onChange={handleInputChange}
+                        {...(errors.email && { error: true, helperText: errors.lastName })}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth
+                        name="contactPerson"
+                        variant="outlined"
+                        label="Contact Person"
+                        value={values.contactPerson}
+                        onChange={handleInputChange}
+                        {...(errors.email && { error: true, helperText: errors.contactPerson })}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth
+                        name="contactNo"
+                        variant="outlined"
+                        label="Contact No"
+                        value={values.contactNo}
+                        onChange={handleInputChange}
+                        {...(errors.email && { error: true, helperText: errors.contactNo })}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth
+                        name="emailAddress"
+                        variant="outlined"
+                        label="Email Address"
+                        value={values.emailAddress}
+                        onChange={handleInputChange}
+                        {...(errors.email && { error: true, helperText: errors.emailAddress })}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth
+                        name="dateOfBirth"
+                        variant="outlined"
+                        label="Date of Birth"
+                        value={values.dateOfBirth}
+                        onChange={handleInputChange}
+                        {...(errors.email && { error: true, helperText: errors.dateOfBirth })}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth
+                        name="age"
+                        variant="outlined"
+                        label="Age"
+                        value={values.age}
+                        onChange={handleInputChange}
+                        {...(errors.email && { error: true, helperText: errors.age })}
+                    />
+                </Grid>
+                <div>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                    >
+                        Submit
+                    </Button>
+                    <Button
+                        variant="contained"
+                        onClick={resetForm}
+                    >
+                        Reset
+                    </Button>
+                </div>
+            </Grid>
+        </ form>
     );
 }
 
